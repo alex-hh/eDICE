86de2b26ae4087fb26430be7613753817078e562
Giovanni Visona <giovanni.visona@tuebingen.mpg.de>
2022-12-16 10:32:07 +0100

    created package branch to develop python install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='eDICE',
+    version='0.1',
+    packages=['edice',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    # long_description=open('README.md').read(),
+)
